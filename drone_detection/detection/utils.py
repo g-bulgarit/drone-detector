f1b87b059d4bb5efe736003a834d1522b3a043c1
g-Bulgarit <alonio6@gmail.com>
2023-10-14 14:45:56 +0300

    fix: moved docstring to correct location

--- drone_detection/detection/utils.py
+++ drone_detection/detection/utils.py
@@ -2,6 +2,26 @@ from ultralytics.engine.results import Results
 
 
 def is_drone(model_res: Results, config: dict) -> bool:
+    """
+    Check if a drone is detected in the model results.
+
+    This function iterates through the bounding boxes in the model results and
+    checks if any of them correspond to a monitored class and meet a minimum
+    detection confidence threshold.
+
+    :param model_res: Results object containing bounding boxes
+                      and their associated data.
+    :type model_res: Results
+
+    :param config: Configuration dictionary with parameters for detection.
+                   It should include 'monitored_classes_ids' for monitored class IDs
+                   and 'detection_confidence_threshold' for the confidence threshold.
+    :type config: dict
+
+    :return: True if a drone is detected based on the specified criteria,
+             False otherwise.
+    :rtype: bool
+    """
     for box in model_res.boxes:
         if box.cls in config["monitored_classes_ids"]:
             if box.conf > config["detection_confidence_threshold"]:
--- drone_detection/image_processing/preprocessing.py
+++ drone_detection/image_processing/preprocessing.py
@@ -3,24 +3,6 @@ from pathlib import Path
 
 
 def preprocess_frame(frame_path: Path, output_dir: Path) -> None:
-    """
-    Check if a drone is detected in the model results.
-
-    This function iterates through the bounding boxes in the model results and
-    checks if any of them correspond to a monitored class and meet a minimum
-    detection confidence threshold.
-
-    :param model_res: Results object containing bounding boxes and their associated data.
-    :type model_res: Results
-
-    :param config: Configuration dictionary with parameters for detection.
-                   It should include 'monitored_classes_ids' for monitored class IDs
-                   and 'detection_confidence_threshold' for the confidence threshold.
-    :type config: dict
-
-    :return: True if a drone is detected based on the specified criteria, False otherwise.
-    :rtype: bool
-    """
     image_frame_count = Path(frame_path).stem
     image = cv2.imread(str(frame_path))
     h, w, _ = image.shape
