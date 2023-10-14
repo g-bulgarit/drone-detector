c111e25f35259efe0232bfe44b4e8b23a3b9c3a8
g-Bulgarit <alonio6@gmail.com>
2023-10-14 13:50:08 +0300

    add: detection script triggered by zmq

---
+++ scripts/detect.py
@@ -0,0 +1,25 @@
+from ultralytics import YOLO
+import zmq
+import json
+
+from drone_detection.detection.utils import is_drone
+from drone_detection.configuration.config import ZMQ_NEW_FILE_PUB_PORT
+
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+socket.connect(f"tcp://127.0.0.1:{ZMQ_NEW_FILE_PUB_PORT}")
+socket.subscribe("")
+
+if __name__ == "__main__":
+    # Load a model
+    model = YOLO("yolov8n.pt")  # pretrained YOLOv8n model
+
+    while True:
+        path_str = socket.recv_string()
+        recv_dict = json.loads(path_str)
+        frame_path: str = recv_dict["path"]
+
+        # Run batched inference on a list of images
+        results = model(frame_path)  # return a list of Results objects
+        config = dict(monitored_classes_ids=[5], detection_confidence_threshold=0.8)
+        is_drone(results[0], config=config)
