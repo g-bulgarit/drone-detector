f49902853066b9953e2c162e28ea507022f8480d
Amir Sarig <amir-s@Amirs-MacBook-Air.local>
2023-10-14 12:45:11 +0300

    preliminary yolo ultralytics support

---
+++ drone_detection/detection/utils.py
@@ -0,0 +1,8 @@
+from ultralytics.engine.results import Results
+
+
+def is_drone(model_res: Results, config: dict) -> bool:
+    for box in model_res.boxes:
+        if box.cls in config['monitored_classes_ids']:
+            if box.conf > config['detection_confidence_threshold']:
+                print("Drone Detected!")
