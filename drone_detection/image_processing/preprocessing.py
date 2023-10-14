ddf8fdea4118ae3a12a15b73e09cbc4ae308c413
Amir Sarig <amir-s@Amirs-MacBook-Air.local>
2023-10-14 12:54:00 +0300

    added docstring

--- drone_detection/image_processing/preprocessing.py
+++ drone_detection/image_processing/preprocessing.py
@@ -3,6 +3,24 @@ from pathlib import Path
 
 
 def preprocess_frame(frame_path: Path, output_dir: Path) -> None:
+    """
+    Check if a drone is detected in the model results.
+
+    This function iterates through the bounding boxes in the model results and
+    checks if any of them correspond to a monitored class and meet a minimum
+    detection confidence threshold.
+
+    :param model_res: Results object containing bounding boxes and their associated data.
+    :type model_res: Results
+
+    :param config: Configuration dictionary with parameters for detection.
+                   It should include 'monitored_classes_ids' for monitored class IDs
+                   and 'detection_confidence_threshold' for the confidence threshold.
+    :type config: dict
+
+    :return: True if a drone is detected based on the specified criteria, False otherwise.
+    :rtype: bool
+    """
     image_frame_count = Path(frame_path).stem
     image = cv2.imread(str(frame_path))
     h, w, _ = image.shape
@@ -12,6 +30,6 @@ def preprocess_frame(frame_path: Path, output_dir: Path) -> None:
     for y in range(0, h, tiley):
         for x in range(0, w, tilex):
             output_filename = f"{output_dir}/{image_frame_count}_{tile_counter}.jpg"
-            tile = image[y : y + tiley, x : x + tilex]
+            tile = image[y: y + tiley, x: x + tilex]
             cv2.imwrite(filename=output_filename, img=tile)
             tile_counter += 1
